9f3ac3209383c308801a74aecccdc9314a86a270
Ritesh <riteshkatukojwala@gmail.com>
2022-09-01 07:13:08 +0530

    opcodes

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "stdint.h"
+//maximum memory in LC3 is 65536
+#define MEMORY_MAX (1<<16)
+uint16_t memory[MEMORY_MAX];
+
+
+//Registers
+//here enum variables have global scope
+enum{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC,//program counter (stores address of next instruction to be executed)
+    R_COND,
+    R_COUNT
+};
+uint16_t reg[R_COUNT];
+
+//Opcodes
+//operation codes are the atomic tasks cpu can perform 
+//here opcodes are 16 bit unsigned integers
+enum{
+    OP_BR = 0, //branch
+    OP_ADD, //add
+    OP_LD, //load
+    OP_ST, //store
+    OP_JSR, //jump register
+    OP_AND, //bitwise and
+    OP_LDR, //load register
+    OP_STR, //store register
+    OP_RTI, //unused
+    OP_NOT, // bitwise not
+    OP_LDI, // load indirect
+    OP_STI, // store indirect 
+    OP_JMP, // jump
+    OP_RES, // reserved
+    OP_LEA, // load effective address
+    OP_TRAP, // executive trap
+};
